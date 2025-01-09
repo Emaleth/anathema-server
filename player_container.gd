6fa9f7701fcaa07167da14d63a836cc3991ad1f9
Emaleth <Emaleth@protonmail.com>
2025-01-09 16:04:14 +0100

    update

--- player_container.gd
+++ player_container.gd
@@ -1,4 +1,4 @@
 extends Node
 
 
-var player_name : String
+var player_name : String = "e"
--- player_verification.gd
+++ player_verification.gd
@@ -1,11 +1,18 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var player_container_scene = preload("res://player_container.tscn")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func start(player_id):
+	CreatePlayerContainer(player_id)
+
+
+func CreatePlayerContainer(player_id):
+	var new_player_container = player_container_scene.instantiate()
+	new_player_container.name = str(player_id)
+	get_parent().add_child(new_player_container, true)
+	FillPlayerContainer(player_id)
+
+func FillPlayerContainer(player_id):
+	print(get_parent().get_node(str(player_id)).player_name)
